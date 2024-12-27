a64e34d6cdb8a8421345c2365e344b0ee221477f
AvniGupta101 <avngupta06@gmail.com>
2024-12-27 14:04:33 +0530

    Update Home.jsx

--- HC-website/src/assets/pages/Home/Home.jsx
+++ HC-website/src/assets/pages/Home/Home.jsx
@@ -13,11 +13,8 @@ function Home() {
         <div className="header-box-container">
           <header className="header-box">
             <h1 className="text-center homepage-title">Android App Development Course</h1>
-            <p className="text-center homepage-description">
-             
-            </p>
+            <p className="text-center homepage-description"></p>
             <div className="course-details text-center">
-              {/* <p><strong>Android App Development Course</strong></p> */}
               <p>With Kotlin and Jetpack Compose</p>
               <p>15 days training program</p>
               <p>For beginners and experienced developers, UG/PG students</p>
@@ -31,7 +28,7 @@ function Home() {
             <div className="col-12">
               {/* Replace the Registration Link box with Program Outcomes */}
               <HomeBox heading="Program Outcomes" content={
-                <ul className="list-unstyled">
+                <ul className="custom-bullet-list">
                   <li>Gain hands-on experience with Kotlin and Jetpack Compose for Android app development.</li>
                   <li>Explore advanced concepts like Firebase integration, MVVM architecture, APIs, and real-time communication.</li>
                   <li>Learn to design visually stunning and user-friendly app interfaces.</li>
@@ -61,8 +58,6 @@ function Home() {
         <br /><br /><br />
         <h4 className="text-center">Feel free to reach out at</h4>
         <p className="text-center">executives@humblecoders.in </p>
-        
-
         <br /><br /><br /><br />
       </div>
       <SiteFooter />
