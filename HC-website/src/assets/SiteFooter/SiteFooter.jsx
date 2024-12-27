2c4d39ad1a8fa04a12636422eef08879aa75570c
AvniGupta101 <avngupta06@gmail.com>
2024-12-27 14:07:06 +0530

    Update SiteFooter.jsx

--- HC-website/src/assets/SiteFooter/SiteFooter.jsx
+++ HC-website/src/assets/SiteFooter/SiteFooter.jsx
@@ -1,16 +1,23 @@
 // SiteFooter.js
 import React from 'react';
-import './SiteFooter.css'; // For styling the footer
+import './SiteFooter.css';
+import logo from './hc_logo.png'; // For styling the footer
 
 const SiteFooter = () => {
     return (
         <footer className="site-footer">
-            <p>
+             <img src={logo} alt="Logo" className="footer-logo" />
+            <p >
+           
             Developed by
             <strong> <a href="https://www.linkedin.com/in/nishesh-singla-ba0574284/" target="_blank" rel="noopener noreferrer"> Nishesh</a></strong>
             {" "}and{" "}
             <strong> <a href="https://www.linkedin.com/in/avni-gupta-235b3429a/" target="_blank" rel="noopener noreferrer"> Avni</a></strong>
+            
+                
+            
             </p>
+            
         </footer>
     );
 };
